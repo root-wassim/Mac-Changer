1ad19bb41be0ab7b9dc1a6a59db9ec4572085982
wassim2005x <faycalwassim.daoudi@etu.usthb.dz>
2025-11-04 23:25:23 +0100

    V2.0 Developing the code structure and improving security

--- wmct.py
+++ wmct.py
@@ -4,39 +4,59 @@
 import subprocess
 import optparse
 import re
+import sys
 
-parser = optparse.OptionParser()
-parser.add_option("-i","--interface", dest="network_interface", help="The place to put the network interface")
-parser.add_option("-m","--mac", dest="new_mac",help="The place to put the new mac address")
+def get_argument():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface", dest="network_interface", help="The place to put the network interface")
+    parser.add_option("-m","--mac", dest="new_mac",help="The place to put the new mac address")
 
-options, arguments = parser.parse_args()
-
-
-if not options.network_interface and not options.new_mac :
-    print("[-] Specify an interface and mac address ")
-    exit()
-
-elif not options.new_mac :
-    print("[-] Specify a mac address ")
-    exit()
-
-elif not options.network_interface :
-    print("[-] Specify an interface  ")
-    exit()
+    options, arguments = parser.parse_args()
 
 
-subprocess.call("ifconfig " + options.network_interface + " down", shell=True)
-subprocess.call("ifconfig " + options.network_interface + " hw ether " + options.new_mac, shell=True)
-subprocess.call("ifconfig " + options.network_interface + " up", shell=True)
+    if not options.network_interface and not options.new_mac :
+        parser.error("[-] Specify an interface and mac address , type -h for help")
+    
+    elif not options.new_mac :
+        parser.error("[-] Specify a mac address , type -h for help")
 
-print("[+] Changing MAC Address for " + options.network_interface + " to " + options.new_mac)
+    elif not options.network_interface :
+        parser.error("[-] Specify an interface  , type -h for help")
 
-ifconfig_check = subprocess.check_output("ifconfig " + options.network_interface, shell=True).decode("UTF-8")
-check = re.search(r"(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)", ifconfig_check)
+    return options    
 
 
-if check[0] == options.new_mac :
-    print("[+] Mac address has changed successfully")
 
-else :
-    print("[-] Changing mac to " + options.new_mac + " was failled")    
+def change_mac(network_interface ,new_mac):
+    try:
+        subprocess.check_call(["ifconfig", network_interface, "down"])
+        subprocess.check_call(["ifconfig", network_interface, "hw", "ether", new_mac])
+        subprocess.check_call(["ifconfig", network_interface, "up"])
+    except subprocess.CalledProcessError as e:
+        print(f"[-] Failed to run ifconfig commands: {e}")
+        sys.exit(1)
+
+    print("[+] Changing MAC Address for " + network_interface + " to " + new_mac)
+
+
+
+def check_mac(network_interface ,new_mac):
+    try:
+        ifconfig_check = subprocess.check_output(["ifconfig", network_interface]).decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        print(f"[-] Failed to run ifconfig: {e}")
+        sys.exit(1)
+
+    check = re.search(r"(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)", ifconfig_check)
+
+    if check[0] == new_mac :
+        print("[+] Mac address has changed successfully")
+    else :
+        print("[-] Changing mac to " + new_mac + " was failled")  
+
+
+if __name__ == "__main__":
+   
+   options = get_argument()
+   change_mac(options.network_interface,options.new_mac)
+   check_mac(options.network_interface,options.new_mac)   
